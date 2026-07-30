f3c7fcf6ea3b62be05916384c1ff68c240832258
sukhmanikaur61 <sukhmanikaurneo@gmail.com>
2026-07-30 10:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ Resume with Latest Job apply links""")
 
 st.image("bg.png")
 
+st.sidebar.title("Fill Important Details")
+st.sidebar.image("bg.png")
+
 
 
 # ==============API keys================
